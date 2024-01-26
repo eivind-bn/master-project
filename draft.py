8bfa22f0d3f593892aebfe801f31b63d934fb068
Eivind <eivind.bergernilsen@gmail.com>
2024-01-26 16:17:48 +0100

    working on policy class

--- draft.py
+++ draft.py
@@ -7,20 +7,39 @@ from xai.asteroids import *
 from xai.window import *
 from xai.action import *
 from xai.angle import *
+from torch.optim import Optimizer, SGD, Adam
+from torch.nn import Sequential, Linear, ReLU
+from torch import tensor, Tensor
+from xai.tensor import *
+from xai.policy import Policy
+from torchvision.datasets.mnist import MNIST
+import matplotlib.pyplot as plt
+# %%
+mnist = MNIST(".", download=True)
+mnist.data = mnist.data.float()
+mnist.data /= 255.0
+mnist.data = mnist.data.cuda()
+mnist.data
+# %%
+
+labels = torch.zeros((mnist.data.shape[0],10)).float()
+labels[torch.arange(0,mnist.data.shape[0]),mnist.targets] = 1.0
+labels = labels.cuda()
+labels
+
 
 # %%
+
+f = Policy(28*28,10, device="auto")
+f.fit(mnist.data.flatten(start_dim=1), labels, 5000, 32, "adam", optimizer_params={
+})
+# %%
+
+plt.imshow(mnist.data[600].cpu().numpy()), f(mnist.data[600].flatten())
+# %%
+
+
 env = Asteroids()
 env.play(show=True, translate=False, rotate=False, fps=60, stochastic=False)
 # %%
-Actions.NOOP
-# %%
-
-l = list(AngleStates)
-l
-# %%
-
-
-x = Radians(2) + Radians(4)
-x += Radians(2)
-x
 # %%
--- xai/policy.py
+++ xai/policy.py
@@ -1,12 +1,35 @@
 from typing import *
 from typing import Any
 from torch import Tensor
+from torch.nn import Sequential
+from torch.optim import Optimizer, SGD, Adam
 from .feed_forward import FeedForward
 
 import torch
 
 Device = Literal["cpu", "cuda", "cuda:0", "cuda:1", "auto"]
-Optimizer = Literal["sgd", "adam"]
+OptimizerType = Literal["sgd", "adam"]
+
+class SgdParams(TypedDict, total=False):
+    lr: float
+    momentum: float
+    dampening: float
+    weight_decay: float
+    nesterov: bool
+
+class AdamParams(TypedDict, total=False):
+    lr: float|Tensor
+    betas: Tuple[float, float]
+    eps: float
+    weight_decay: float
+    amsgrad: bool
+    foreach: bool
+    maximize: bool
+    capturable: bool
+    differentiable: bool
+    fused: bool
+
+OptimizerParams = SgdParams|AdamParams
 
 class Policy:
 
@@ -14,15 +37,83 @@ class Policy:
                  input_dim: int,
                  output_dim: int,
                  hidden_layers: int|Sequence[int] = 2,
+                 optimizer_params: OptimizerParams|None = None,
                  device: Device = "auto") -> None:
         super().__init__()
 
         if device == "auto":
-            self.device: Device = "cuda" if torch.cuda.is_available() else "cpu"
+            self.device = "cuda" if torch.cuda.is_available() else "cpu"
         else:
             self.device = device
 
-        self._net = torch.nn.Sequential()
+        self._net = self._build_network(
+            input_dim=input_dim,
+            output_dim=output_dim,
+            hidden_layers=hidden_layers,
+            device=self.device
+        )
+
+        self._optimizers: Dict[OptimizerType,Optimizer] = {
+            "sgd": torch.optim.SGD(self._net.parameters(), lr=0.1),
+            "adam": torch.optim.Adam(self._net.parameters())
+        }
+
+        self._set_optim_param()
+
+        self._loss_functions: Dict[LossType,object] = {
+            "mse": torch.nn.MSELoss()
+        }
+        
+    def predict(self, 
+                x:              Tensor|FeedForward, 
+                move_to_device: bool = False) -> FeedForward:
+        
+        if isinstance(x, FeedForward):
+            x = x._output
+
+        if move_to_device:
+            x = x.to(device=self.device)
+
+        return FeedForward(
+            input=x,
+            network=self._net
+        )
+    
+    def __call__(self, x: Tensor|FeedForward) -> FeedForward:
+        return self.predict(x)
+    
+    def fit(self, 
+            X:                  Tensor, 
+            Y:                  Tensor, 
+            steps:              int, 
+            batch_size:         int,
+            optimizer_type:     OptimizerType, 
+            optimizer_params:   OptimizerParams|None = None) -> None:
+        
+        optimizer = self._optimizers[optimizer_type]
+
+        if optimizer_params:
+            self._set_optim_param(optimizer, optimizer_params)
+        
+        for step in range(steps):
+            optimizer.zero_grad()
+            x,y = self._mini_batch(X, Y, batch_size)
+            y_hat = self(x).tensor()
+            loss = torch.nn.functional.mse_loss(y_hat, y)
+            print(loss)
+            loss.backward()
+            optimizer.step()
+            
+    def save(self, path: str) -> None:
+        torch.save(self, path)
+
+    @staticmethod
+    def _build_network(input_dim: int,
+                       output_dim: int,
+                       hidden_layers: int|Sequence[int] = 2,
+                       device: Device = "auto") -> Sequential:
+
+        net = torch.nn.Sequential()
 
         I, O = input_dim, output_dim
 
@@ -35,45 +126,32 @@ class Policy:
             raise TypeError(f"Argument for layers has incompatible type: {type(hidden_layers)}")
         
         for i in range(1, len(layers)-1):
-            self._net += torch.nn.Sequential(
-                torch.nn.Linear(layers[i-1], layers[i], device=self.device),
+            net += torch.nn.Sequential(
+                torch.nn.Linear(layers[i-1], layers[i], device=device),
                 torch.nn.ReLU()
             )
 
-        self._net += torch.nn.Sequential(torch.nn.Linear(layers[-2], layers[-1], device=self.device))
-        
-    def predict(self, x: Tensor|FeedForward) -> FeedForward:
-        if isinstance(x, Tensor):
-            x = x.to(self.device)
-        else:
-            x = x._output.to(self.device)
+        net += torch.nn.Sequential(torch.nn.Linear(layers[-2], layers[-1], device=device))
 
-        return FeedForward(
-            input=x,
-            network=self._net
-        )
-    def __call__(self, x: Tensor|FeedForward) -> FeedForward:
-        return self.predict(x)
+        return net
     
-    def fit(self, 
-            x:              Tensor, 
-            y:              Tensor, 
-            steps:          int, 
-            learning_rate:  float, 
-            optimizer:      Optimizer) -> None:
-        match optimizer:
-            case "sgd":
-                self._sgd_fit()
-            case "adam":
-                self._adam_fit()
-            case _:
-                raise ValueError(f"Optimizer: {optimizer} is invalid.")
-            
-    def save(self, path: str) -> None:
-        torch.save(self, path)
+    def _mini_batch(self,
+                    X: Tensor, 
+                    Y: Tensor, 
+                    batch_size: int, 
+                    sample_dim: int = 0) -> Tuple[Tensor,Tensor]:
+        assert X.shape[0] == Y.shape[0] and batch_size > 0
+        idx = torch.randperm(min(batch_size, X.shape[sample_dim]), device=self.device)
+        return X.index_select(sample_dim, idx), Y.index_select(sample_dim, idx)
+    
+    @staticmethod
+    def _set_optim_param(optimizer: Optimizer, params: OptimizerParams) -> None:
+        for param_group in optimizer.param_groups:
+            for param,value in params.items():
+                param_group[param] = value
 
     @staticmethod
-    def load(path: str) -> "Policy":
+    def load(path: str) -> "Policy"|NoReturn:
         policy: Policy = torch.load(path)
         if isinstance(policy, Policy):
             return policy
