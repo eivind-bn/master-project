e611f69d106c4c440842c35c870e7b65fd794a45
eivind <eivind.bergernilsen@gmail.com>
2024-02-02 17:54:14 +0100

    policy working. added mypy.ini file

---
+++ xai/loss.py
@@ -0,0 +1,111 @@
+from typing import *
+from typing import Any
+from torch import Tensor
+from torch.nn import Module
+
+import torch
+import warnings
+
+LossName = Literal[
+    "L1Loss",
+    "MSELoss",
+    "CrossEntropyLoss",
+    #"CTCLoss",
+    #"NLLLoss",
+    "PoissonNLLLoss",
+    #"GaussianNLLLoss",
+    "KLDivLoss",
+    "BCELoss",
+    "BCEWithLogitsLoss",
+    #"MarginRankingLoss",
+    "HingeEmbeddingLoss",
+    #"MultiLabelMarginLoss",
+    "HuberLoss",
+    "SmoothL1Loss",
+    "SoftMarginLoss",
+    "MultiLabelSoftMarginLoss",
+    #"CosineEmbeddingLoss",
+    #"MultiMarginLoss",
+    #"TripletMarginLoss",
+    #"TripletMarginWithDistanceLoss",
+]
+LossSelector = Callable[[Type["LossModule"]],"LossModule"]
+LossFunction = Callable[[Tensor,Tensor],Tensor]
+
+T = TypeVar("T")
+P = ParamSpec("P")
+
+class LossType(Generic[P]):
+
+    def __init__(self, loss_module: Callable[P,Module]) -> None:
+        super().__init__()
+        self.loss_module = loss_module
+
+    def __call__(self, *args: P.args, **kwargs: P.kwargs) -> "LossModule":
+        return LossModule(self.loss_module(*args, **kwargs))
+    
+
+class LossModule(Generic[P]):
+    L1Loss = LossType(torch.nn.L1Loss)
+    MSELoss = LossType(torch.nn.MSELoss)
+    CrossEntropyLoss = LossType(torch.nn.CrossEntropyLoss)
+    #CTCLoss = LossType(torch.nn.CTCLoss)
+    #NLLLoss = LossType(torch.nn.NLLLoss)
+    PoissonNLLLoss = LossType(torch.nn.PoissonNLLLoss)
+    #GaussianNLLLoss = LossType(torch.nn.GaussianNLLLoss)
+    KLDivLoss = LossType(torch.nn.KLDivLoss)
+    BCELoss = LossType(torch.nn.BCELoss)
+    BCEWithLogitsLoss = LossType(torch.nn.BCEWithLogitsLoss)
+    #MarginRankingLoss = LossType(torch.nn.MarginRankingLoss)
+    HingeEmbeddingLoss = LossType(torch.nn.HingeEmbeddingLoss)
+    #MultiLabelMarginLoss = LossType(torch.nn.MultiLabelMarginLoss)
+    HuberLoss = LossType(torch.nn.HuberLoss)
+    SmoothL1Loss = LossType(torch.nn.SmoothL1Loss)
+    SoftMarginLoss = LossType(torch.nn.SoftMarginLoss)
+    MultiLabelSoftMarginLoss = LossType(torch.nn.MultiLabelSoftMarginLoss)
+    #CosineEmbeddingLoss = LossType(torch.nn.CosineEmbeddingLoss)
+    #MultiMarginLoss = LossType(torch.nn.MultiMarginLoss)
+    #TripletMarginLoss = LossType(torch.nn.TripletMarginLoss)
+    #TripletMarginWithDistanceLoss = LossType(torch.nn.TripletMarginWithDistanceLoss)
+
+    def __init__(self, loss_module: Module|Callable[[Tensor,Tensor],Tensor]) -> None:
+        super().__init__()
+        self._loss_module = loss_module
+
+    def __call__(self, input: Tensor, target: Tensor) -> Tensor:
+        return cast(Tensor, self._loss_module(input, target))
+
+    @classmethod
+    def types(cls) -> Iterator[Tuple[str,"LossType"]]:
+        for name,var in vars(cls).items():
+            if isinstance(var, LossType):
+                yield name, var
+
+    @overload
+    @classmethod
+    def get(cls, loss_name: LossName, *, default: T) -> "LossType"|T: ...
+
+    @overload
+    @classmethod
+    def get(cls, loss_name: LossName) -> "LossType": ...
+
+    @classmethod
+    def get(cls, loss_name: LossName, **kwargs: T) -> "LossType"|T:
+        for name,module_type in cls.types():
+            if name == loss_name:
+                return module_type
+        
+        if "default" in kwargs:
+            return kwargs["default"]
+        else:
+            raise KeyError(f"Loss-type: {loss_name} not found.")
+
+# Verify that each loss function accepts two arguments once instantiated.
+for name,module_type in LossModule.types():
+    try:
+        test_tensor = torch.ones((5,), dtype=torch.float32, device="cpu")
+        with warnings.catch_warnings(action="ignore"):
+            module = module_type() 
+            loss = module(test_tensor,test_tensor)
+    except Exception as e:
+        raise ValueError(f"Incompatible module: {name}")
--- xai/optimizer.py
+++ xai/optimizer.py
@@ -1,107 +1,135 @@
 from typing import *
 from abc import ABC, abstractmethod
+from typing import Any, Callable
 from torch import Tensor
+from torch.nn import Parameter, Module
 from tqdm import tqdm
 from torch.optim import Optimizer as TorchOptimizer
-from .policy import Policy
+from .loss import LossType, LossModule, LossFunction, LossName, LossSelector
 
 import torch
+import inspect
 
-P = TypeVar("P", bound=Dict[str,Any])
+T = TypeVar("T", contravariant=True)
+P = TypeVar("P", covariant=True)
 
-class Optimizer(ABC, Generic[P]):
+class ParamCall(Protocol[T,P]):
+    def __call__(self, step: int, params: T) -> P: ...
 
-    def __init__(self, policy: Policy) -> None:
-        self._policy = policy
+Param = Union[P,ParamCall[T,P]]
+
+class Optimizer(ABC):
+
+    def __init__(self, 
+                 optimizer_type:    Type[TorchOptimizer], 
+                 trainable_params:  Iterable[Parameter], 
+                 forward:           Callable[[Tensor],Tensor],
+                 **hyperparameters: Any) -> None:
+        
+        self._optimizer_type = optimizer_type
         self._optimizer: TorchOptimizer|None = None
+        self._f_params = trainable_params
+        self._f = forward
 
-    @abstractmethod
-    def get_optimizer(self, **params: Unpack[P]) -> TorchOptimizer: # type: ignore[misc]
-        pass
+        self._param_values: Dict[str,Any] = {}
+        self._param_callables: Dict[str,ParamCall[Any,Any]] = {}
+
+        for name,param in hyperparameters.items():
+            if callable(param):
+                self._param_callables[name] = param
+            else:
+                self._param_values[name] = param
+
+    def get_optimizer(self, step: int) -> TorchOptimizer:
+        if self._optimizer is None:
+            params = self._param_values.copy()
+
+            for key,callable_value in self._param_callables.items():
+                params[key] = callable_value(step, params)
+
+            self._optimizer = self._optimizer_type(self._f_params, **params)
+        else:
+            for param_group in self._optimizer.param_groups:
+
+                for key,value in self._param_values.items():
+                    param_group[key] = value
+
+                for key,callable_value in self._param_callables.items():
+                    param_group[key] = callable_value(step, param_group)
+
+        return self._optimizer
 
     def fit(self, 
-            X:          Tensor, 
-            Y:          Tensor, 
-            steps:      int, 
-            batch_size: int,
-            verbose:    bool = False, 
-            **params:   Unpack[P]) -> None: # type: ignore[misc]
+            X:              Tensor, 
+            Y:              Tensor, 
+            steps:          int, 
+            batch_size:     int,
+            loss_criterion: LossName|LossSelector|LossFunction|LossModule,
+            verbose:        bool = False) -> None:
         assert X.shape[0] == Y.shape[0] and 0 < batch_size <= X.shape[0]
 
-        optimizer = self.get_optimizer(**params)
+        if isinstance(loss_criterion, LossModule):
+            loss_function = loss_criterion
+        elif isinstance(loss_criterion, str):
+            loss_function = LossModule.get(loss_criterion)()
+        elif callable(loss_criterion):
+            args = inspect.getfullargspec(loss_criterion).args
+            match len(args):
+                case 1:
+                    loss_function = cast(LossSelector, loss_criterion)(LossModule)
+                case 2:
+                    loss_function = LossModule(cast(LossFunction, loss_criterion))
+                case _:
+                    raise TypeError(f"Callable must accept either 1 or 2 arguments, but accepts {len(args)}")
+        else:
+            raise TypeError(f"Incompatible loss criterion: {type(loss_criterion)}")
 
+        
         def mini_batch() -> Tuple[Tensor,Tensor]:
-            idx = torch.randperm(X.shape[0], device=self._policy.device)[:batch_size]
+            idx = torch.randperm(X.shape[0], device=X.device)[:batch_size]
             return X[idx], Y[idx]
 
         with tqdm(total=steps, desc="Step:", disable=not verbose) as bar:
-            for _ in range(steps):
+            for step in range(steps):
+                optimizer = self.get_optimizer(step)
                 optimizer.zero_grad()
                 x,y = mini_batch()
-                loss = self._policy(x).loss(y)
+                y_hat = self._f(x)
+                loss: Tensor = loss_function(y_hat, y)
                 loss.backward()
                 optimizer.step()
                 bar.set_description(f"Loss: {loss:.6f}")
                 bar.update()
 
-class SGDParams(TypedDict, total=False):
-    lr:             float
-    weight_decay:   float
-    momentum:       float
-    dampening:      float
-    nesterov:       bool
+class SGD(Optimizer):
+    class Params(TypedDict, total=False):
+        lr:             Required[Param[float,"SGD.Params"]]
+        momentum:       Param[float,"SGD.Params"]
+        dampening:      Param[float,"SGD.Params"]
+        weight_decay:   Param[float,"SGD.Params"]
+        nesterov:       Param[bool,"SGD.Params"]
 
-class SGD(Optimizer[SGDParams]):
-
-          
-    def get_optimizer(self, **params: Unpack[SGDParams]) -> TorchOptimizer: # type: ignore[override]
-        if self._optimizer is None:
-            self._optimizer = torch.optim.SGD(self._policy.network.parameters(), **params)
-        else:
-            for param_group in self._optimizer.param_groups:
-                param_group.update(params)
-        return self._optimizer
-    
-    def fit(self,  # type: ignore[override]
-            X:          Tensor, 
-            Y:          Tensor, 
-            steps:      int, 
-            batch_size: int, 
-            verbose:    bool = False, 
-            **params:   Unpack[SGDParams]) -> None:
-        return super().fit(X, Y, steps, batch_size, verbose=verbose, **params)
-
-class AdamParams(TypedDict, total=False):
-    lr:                float|Tensor
-    weight_decay:      float
-    betas:             Tuple[float,float]
-    eps:               float
-    amsgrad:           bool
-    foreach:           bool|None
-    maximize:          bool
-    capturable:        bool
-    differentiable:    bool
-    fused:             bool|None  
+    def __init__(self, 
+                 trainable_params:  Iterable[Parameter], 
+                 forward:         Callable[[Tensor], Tensor], 
+                 **params:  Unpack[Params]) -> None:
+        super().__init__(torch.optim.SGD, trainable_params, forward, **params)
 
 class Adam(Optimizer):
+    class Params(TypedDict, total=False):
+        lr:             Param[float|Tensor,"Adam.Params"]
+        betas:          Param[Tuple[float,float],"Adam.Params"]
+        eps:            Param[float,"Adam.Params"]
+        weight_decay:   Param[float,"Adam.Params"]
+        amsgrad:        Param[bool,"Adam.Params"]
+        foreach:        Param[bool|None,"Adam.Params"]
+        maximize:       Param[bool,"Adam.Params"]
+        capturable:     Param[bool,"Adam.Params"]
+        differentiable: Param[bool,"Adam.Params"]
+        fused:          Param[bool|None,"Adam.Params"]
 
-
-    def get_optimizer(self, # type: ignore[override]
-                      **params: Unpack[AdamParams]) -> TorchOptimizer:
-        if self._optimizer is None:
-            self._optimizer = torch.optim.Adam(self._policy.network.parameters(), **params)
-        else:
-            for param_group in self._optimizer.param_groups:
-                param_group.update(params)
-
-        return self._optimizer
-    
-    def fit(self, # type: ignore[override]
-            X:          Tensor, 
-            Y:          Tensor, 
-            steps:      int, 
-            batch_size: int, 
-            verbose:    bool = False, 
-            **params:   Unpack[AdamParams]) -> None:
-        return super().fit(X, Y, steps, batch_size, verbose=verbose, **params)
-    
+    def __init__(self, 
+                 trainable_params:  Iterable[Parameter], 
+                 forward:         Callable[[Tensor], Tensor], 
+                 **params:  Unpack[Params]) -> None:
+        super().__init__(torch.optim.Adam, trainable_params, forward, **params)
