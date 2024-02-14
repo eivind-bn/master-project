7e4b0ee0b3ab20922103bc73cebb2ab60a78979a
eivind <eivind.bergernilsen@gmail.com>
2024-02-14 12:41:08 +0100

    added overloads in stream. refactoring fitness

--- xai/fitness.py
+++ xai/fitness.py
@@ -1,20 +1,15 @@
 from typing import *
 
+from .stream import Stream
+
 class Fitness:
 
     def __init__(self, 
-                 rewards:     Dict[str,float]|None = None,
-                 penalties:   Dict[str,float]|None = None) -> None:
+                 rewards:     Iterable[Tuple[str,float]] = Stream.empty(),
+                 penalties:   Iterable[Tuple[str,float]] = Stream.empty()) -> None:
         
-        if rewards is None:
-            self._named_rewards: Dict[str,float] = {}
-        else:
-            self._named_rewards = rewards.copy()
-
-        if penalties is None:
-            self._named_penalties: Dict[str,float] = {}
-        else:
-            self._named_penalties = penalties.copy()
+        self._rewards = tuple(rewards)
+        self._penalties = tuple(penalties)
 
     def normalized(self, min_fitness: "Fitness", max_fitness: "Fitness") -> "NormalizedFitness":
         normalized_fitness = NormalizedFitness(non_normalized=self)
@@ -34,80 +29,48 @@ class Fitness:
 
         return normalized_fitness
     
-    def _reduce(self, operand: "Fitness", operation: Callable[[float,float],float]) -> "Fitness":
-        result = self.copy()
-        for category,reward in operand.rewards():
-            result._named_rewards[category] = result._named_rewards.get(category, 0) + reward
-
-        for category,penalty in operand.penalties():
-            result._named_penalties[category] = result._named_penalties.get(category, 0) + penalty
-
-        return result
+    def _reduce(self, other: "Fitness", op: Callable[[float,float],float]) -> "Fitness":
+        return Fitness(
+            rewards=self.rewards().chain(other.rewards()).dict(reduce=lambda z1,z2: op(z1,z2)).items(),
+            penalties=self.penalties().chain(other.penalties()).dict(reduce=lambda z1,z2: op(z1,z2)).items()
+        )
 
     def __add__(self, other: "Fitness") -> "Fitness":
-        result = self.copy()
-        for category,reward in other.rewards():
-            result._named_rewards[category] = result._named_rewards.get(category, 0) + reward
-
-        for category,penalty in other.penalties():
-            result._named_penalties[category] = result._named_penalties.get(category, 0) + penalty
-
-        return result
+        return self._reduce(other, lambda x,y: x+y)
     
     def __sub__(self, other: "Fitness") -> "Fitness":
-        result = self.copy()
-        for category,reward in other.rewards():
-            result._named_rewards[category] = result._named_rewards.get(category, 0) + reward
-
-        for category,penalty in other.penalties():
-            result._named_penalties[category] = result._named_penalties.get(category, 0) + penalty
-
-        return result
-
-    def rewards(self) -> Iterator[Tuple[str,float]]:
-        for category,reward in self._named_rewards.items():
-            yield category,reward
-
-    def penalties(self) -> Iterator[Tuple[str,float]]:
-        for category,penalty in self._named_penalties.items():
-            yield category,penalty
+        return self._reduce(other, lambda x,y: x+y)
     
-    def get_reward(self, category: str) -> float:
-        return self._named_rewards.get(category, 0)
+    def __mul__(self, other: "Fitness") -> "Fitness":
+        return self._reduce(other, lambda x,y: x*y)
     
-    def set_reward(self, category: str, reward: float) -> None:
-        self._named_rewards[category] = reward
-    
-    def get_penalty(self, category: str) -> float:
-        return self._named_penalties.get(category, 0)
-    
-    def set_penalty(self, category: str, penalty: float) -> None:
-        self._named_penalties[category] = penalty
+    def __truediv__(self, other: "Fitness") -> "Fitness":
+        return self._reduce(other, lambda x,y: x/y)
+
+    def rewards(self) -> Stream[Tuple[str,float]]:
+        return Stream(self._rewards)
+
+    def penalties(self) -> Stream[Tuple[str,float]]:
+        return Stream(self._penalties)
 
     def __repr__(self) -> str:
         return str(dict(
-            rewards=self._named_rewards,
-            penalties=self._named_penalties
+            rewards=self._rewards,
+            penalties=self._penalties
         ))
-
-    def copy(self) -> "Fitness":
-        return Fitness(
-            rewards=self._named_rewards.copy(),
-            penalties=self._named_penalties.copy()
-        )
 
     @staticmethod
     def max(fitnesses: Iterable["Fitness"]) -> "Fitness":
         max_fitness = Fitness()
         for fitness in fitnesses:
             for category,reward in fitness.rewards():
-                if category in max_fitness._named_rewards:
+                if category in max_fitness._rewards:
                     max_fitness.set_reward(category, max(max_fitness.get_reward(category), reward))
                 else:
                     max_fitness.set_reward(category, reward)
 
             for category,penalty in fitness.penalties():
-                if category in max_fitness._named_penalties:
+                if category in max_fitness._penalties:
                     max_fitness.set_penalty(category, max(max_fitness.get_penalty(category), penalty))
                 else:
                     max_fitness.set_penalty(category, penalty)
@@ -119,13 +82,13 @@ class Fitness:
         min_fitness = Fitness()
         for fitness in fitnesses:
             for category,reward in fitness.rewards():
-                if category in min_fitness._named_rewards:
+                if category in min_fitness._rewards:
                     min_fitness.set_reward(category, min(min_fitness.get_reward(category), reward))
                 else:
                     min_fitness.set_reward(category, reward)
 
             for category,penalty in fitness.penalties():
-                if category in min_fitness._named_penalties:
+                if category in min_fitness._penalties:
                     min_fitness.set_penalty(category, min(min_fitness.get_penalty(category), penalty))
                 else:
                     min_fitness.set_penalty(category, penalty)
