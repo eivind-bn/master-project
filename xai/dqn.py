4d4c091d214f0399446a242dd9a25c20d9fcef3d
Eivind <eivind.bergernilsen@gmail.com>
2024-02-21 18:02:57 +0100

    dqn dev

--- xai/dqn.py
+++ xai/dqn.py
@@ -25,9 +25,89 @@ import random
 import numpy as np
 import copy
 
+class ReplayBatch(NamedTuple):
+    state: Tensor
+    action: Tensor
+    reward: Tensor
+    next_state: Tensor
+    done: Tensor
+
+class ReplayBuffer:
+
+    def __init__(self, size: int, state_shape: Tuple[int,...], actions: Sequence[Action]) -> None:
+        self.state = torch.zeros((size,) + state_shape, dtype=torch.float32, device="cpu")
+        self.action = torch.zeros((size, len(actions)), dtype=torch.float32, device="cpu")
+        self.reward = torch.zeros((size, 1), dtype=torch.float32, device="cpu")
+        self.next_state = torch.zeros((size,) + state_shape, dtype=torch.float32, device="cpu")
+        self.done = torch.zeros((size, 1))
+
+        self._action_to_idx = {action:index for index,action in enumerate(actions)}
+        self._size = size
+        self._current_position = 0
+
+    @overload
+    def append(self, 
+               *,
+               steps:        Iterable[Step]) -> None: ...
+
+    @overload
+    def append(self, 
+               *,
+               state:       Tensor, 
+               action:      Tensor, 
+               reward:      Tensor, 
+               next_state:  Tensor, 
+               done:        Tensor) -> None: ...
+
+    def append(self, 
+               *,
+               steps:       Iterable[Step]|None = None, 
+               state:       Tensor|None = None, 
+               action:      Tensor|None = None, 
+               reward:      Tensor|None = None, 
+               next_state:  Tensor|None = None, 
+               done:        Tensor|None = None) -> None:
+        if steps is None:
+            assert all(arg is not None for arg in (state, action, reward, next_state, done))
+            assert len(state) == len(action) == len(reward) == len(next_state) == len(done)
+
+            indices = torch.arange(self._current_position, self._current_position + len(state), dtype=torch.int32, device="cpu") % self._size
+
+            self.state[indices] = state.to(dtype=torch.float32, device="cpu")
+            self.action[indices] = action.to(dtype=torch.float32, device="cpu")
+            self.reward[indices] = reward.to(dtype=torch.float32, device="cpu")
+            self.next_state[indices] = next_state.to(dtype=torch.float32, device="cpu")
+            self.done[indices] = done.to(dtype=torch.float32, device="cpu")
+
+            if len(indices) > 0:
+                self._current_position = int(indices[-1].item())
+        else:
+            self.append(
+                state=torch.stack([step.observation.translated().rotated().tensor(normalize=True, device="cpu") for step in steps]),
+                action=torch.tensor([self._action_to_idx[step.action] for step in steps], dtype=torch.int32, device="cpu"),
+                reward=torch.tensor([step.reward_sum() for step in steps], dtype=torch.float32, device="cpu"),
+                next_state=torch.stack([step.next_observation.translated().rotated().tensor(normalize=True, device="cpu") for step in steps]),
+                done=torch.tensor([float(step.done) for step in steps], dtype=torch.float32, device="cpu"),
+            )
+
+    def batch(self, size: int, device: Device) -> ReplayBatch:
+        if device == "auto":
+            device = "cuda" if torch.cuda.is_available() else "cpu"
+
+        selections = torch.randperm(len(self.state), device="cpu")[:size]
+
+        return ReplayBatch(
+            state=self.state[selections].to(device=device),
+            action=self.action[selections].to(device=device),
+            reward=self.reward[selections].to(device=device),
+            next_state=self.next_state[selections].to(device=device),
+            done=self.done[selections].to(device=device)
+        )
+
+
 class DQN(Agent):
 
-    def __init__(self, device: Device, translate: bool, rotate: bool) -> None:
+    def __init__(self, device: Device, translate: bool, rotate: bool, replay_buffer_size: int) -> None:
         super().__init__()
         self._translate = translate
         self._rotate = rotate
@@ -60,6 +140,12 @@ class DQN(Agent):
             torch.nn.ReLU(),
             torch.nn.Linear(in_features=16*7*5, out_features=len(self._actions)),
         ).to(device=self._device)
+
+        self._replay_buffer = ReplayBuffer(
+            size=replay_buffer_size,
+            state_shape=(4,210,160,3),
+            actions=self._actions
+        )
 
     @overload
     def state(self, observation: Observation, to_tensor: Literal[False]) -> NDArray[np.float32]: ...
@@ -102,7 +188,7 @@ class DQN(Agent):
         for observation in observations:
             states.append(self.state(observation, to_tensor=True).movedim(2,0))
             if len(states) == states.maxlen:
-                q_values = self._policy(torch.stack(tuple(states)))
+                q_values.append(self._policy(torch.stack(tuple(states))))
 
         if to_action:
             actions: List[Action] = []
@@ -112,110 +198,68 @@ class DQN(Agent):
         else:
             return torch.stack(q_values)
     
-    def rollout(self, env: Asteroids) -> Stream[Step]:
+    def rollout(self, env: Asteroids, exploration_rate: float|Callable[[],float]) -> Stream[Step]:
+        
+        if isinstance(exploration_rate, float):
+            def explore() -> bool:
+                return random.random() < exploration_rate
+        else:
+            def explore() -> bool:
+                return random.random() < exploration_rate()
+
+        observations: Deque[Observation] = deque(maxlen=4)
         def iterator() -> Iterator[Step]:  
-            if not env.running():
-                observation, rewards = env.reset()
-            else:
-                observation = env.observation
-
             while True:
-                action = self.predict(observation)
-                next_observation,rewards = env.step(action)
-                done = not env.running()
-                steps.append(Step(
-                    number=i,
-                    observation=observation,
-                    action=action,
-                    rewards=rewards,
-                    next_observation=next_observation,
-                    done=done
-                ))
+                if env.running() and len(observations) == observations.maxlen:
 
-                if done:
-                    observation, rewards = env.reset()
+                    if explore():
+                        action: Action = random.choice(self._actions)
+                    else:
+                        action = self.predict(observations)[0]
+
+                    observation,rewards = env.step(action)
+                    observations.append(observation)
+                    done = not env.running()
+
+                    yield Step(observations[-2], action, rewards, observations[-1], done)
+
                 else:
-                    observation = next_observation
+                    observation, rewards = env.reset()
+                    observations.append(observation)
+
+        return Stream(iterator())
 
     def train(self, 
-              num_episodes: int,
-              update_target_frequency: int,
-              epsilon:      float,
-              gamma:        float,
-              batch_size:   int,
-              learning_starts: float,
-              buffer_entry_size:  int,
-              replay_buffer_memory: Memory,
-              use_ram: bool|None = None) -> None:
-        env = Asteroids()
-
-        if use_ram is None:
-            use_ram = replay_buffer_memory > GigaBytes(3.0)
+              total_time_steps:         int,
+              learning_rate:            float = 1e-4,
+              learning_starts:          int = 100,
+              buffer_entry_size:        int = 1_000_000,
+              batch_size:               int = 32,
+              tau:                      float = 1.0,
+              gamma:                    float = 0.99,
+              train_frequency:          Tuple[int, Literal["step", "episode"]] = (4, "step"),
+              gradient_steps:           int = 1,
+              target_update_frequency:  Tuple[int, Literal["step", "episode"]] = (10_000, "step"),
+              exploration_rate_decay:   float = 0.1,
+              initial_exploration_rate: float = 1.0,
+              final_exploration_rate:   float = 0.05,
+              verbose:                  bool = True) -> None:
         
-        replay_buffer: Buffer[Tuple[Observation,Action,int,Observation,bool]] = Buffer(
-            entries=(),
-            use_ram=use_ram,
-            eviction_policy="FIFO",
-            max_memory=replay_buffer_memory,
-            max_entries=buffer_entry_size
-        )
-        huber_loss = torch.nn.SmoothL1Loss()
+        loss = torch.nn.HuberLoss()
+        target_net = copy.deepcopy(self._policy)
+        adam = torch.optim.Adam(target_net.parameters(), lr=learning_rate)
 
-        learning_starts = buffer_entry_size*learning_starts
-        time_step = 0
+        exploration_rate = initial_exploration_rate
 
-        with tqdm() as bar:
-            for episode in range(num_episodes):
-                time_step += 1
+        stepper = self.rollout(Asteroids(), lambda: exploration_rate)
+        
+        self._replay_buffer.append(steps=stepper.take(learning_starts))
 
-                
+        for i,step in stepper.enumerate().take(total_time_steps):
+            
 
 
-                if episode % update_target_frequency == 0:
-                    self._policy = policy
-                    policy = copy.deepcopy(self._policy)
-                    optimizer = torch.optim.Adam(policy.parameters())
 
-                episode_reward = 0
-                current_observation, rewards = env.reset()
-                
-                lives = env.lives()
-                while env.lives() == lives:
 
-                    if random.random() < epsilon:
-                        action: Action = random.sample(population=self._actions, k=1)[0]
-                    else:
-                        action = self.predict(current_observation)
-
-                    new_observation, rewards = env.step(action)
-                    reward_sum = sum(reward.value for reward in rewards)
-                    episode_reward += int(reward_sum)
-
-                    small_buffer.append((current_observation, action, reward_sum, new_observation, env.running()))
-                    if len(small_buffer) > 300:
-                        replay_buffer.extend(entries=small_buffer, verbose=True)
-                        small_buffer.clear()
-
-                    current_observation = new_observation
-
-                    if replay_buffer.entry_size() > learning_starts:
-
-                        batch = replay_buffer.randoms(with_replacement=False).take(batch_size).tuple()
-
-                        old_q = torch.stack([self.predict(entry[0], to_action=False) for entry in batch])
-                        actions = torch.nn.functional.one_hot(torch.tensor([entry[1].ale_id for entry in batch], device=self._device), num_classes=len(self._actions))
-                        reward_sums = torch.tensor([entry[2] for entry in batch], device=self._device).reshape((old_q.shape[0],-1))
-                        done = torch.tensor([int(entry[4]) for entry in batch], device=self._device).reshape((old_q.shape[0],-1))
-
-                        with torch.no_grad():
-                            new_q = torch.stack([self.predict(entry[3], to_action=False) for entry in batch])
-
-                        target = reward_sums + torch.mul((gamma * new_q.max(1).values.unsqueeze(1)), 1 - done)
-                        current = old_q.gather(1, actions.long())
-
-                        optimizer.zero_grad()
-                        huber_loss(current, target)
-                        optimizer.step()
-
-                bar.set_description(f"{episode=}, {replay_buffer.entry_size()=}, {episode_reward=}")
-                bar.update()
+            exploration_rate = min(final_exploration_rate, exploration_rate_decay*exploration_rate)
+        
