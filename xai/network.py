9e146645cce5b0b8f24d4e4e57b3bcb5d01d3d51
eivind <eivind.bergernilsen@gmail.com>
2024-03-25 11:30:36 +0100

    testing shap-weights

--- xai/network.py
+++ xai/network.py
@@ -50,8 +50,13 @@ class Network(Generic[Sx,Sy], ABC):
         def gradients(self, to_scalars: Callable[[Tensor],Tensor]) -> Tensor:
             return self.derivative(to_scalars, order=1)
         
-        def explain(self, algorithm: Explainers, background: Array, link: Links = "identity") -> Explanation[Sx,Sy]:
-            return self.parent.explainer(algorithm, background, link).explain(self.input()).tuple()[0]
+        def explain(self, 
+                    algorithm:  Explainers, 
+                    background: Array, 
+                    max_evals:  int|None = None,
+                    logistic:   bool = False) -> Explanation[Sx,Sy]:
+            explainer = self.parent.explainer(algorithm, background, logistic)
+            return explainer.explain(self.input(), max_evals=max_evals).tuple()[0]
         
         def __call__(self) -> Tensor:
             return self.output()
@@ -101,12 +106,15 @@ class Network(Generic[Sx,Sy], ABC):
             info=info
         )
     
-    def explainer(self, type: Explainers, background: Array, link: Links = "identity") -> Explainer[Sy,Sx]:
+    def explainer(self, 
+                  type:         Explainers, 
+                  background:   Array, 
+                  logistic:     bool = False) -> Explainer[Sy,Sx]:
         return Explainer.new(
             type=type,
             network=self,
             background=background,
-            link=link
+            logistic=logistic
         )
     
     def sgd(self, **params: Unpack[SGD.Params]) -> SGD:
